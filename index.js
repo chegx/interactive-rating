730cf2ce1bf8a9051054fd45334d1701470d439b
chegx <110959546+chegx@users.noreply.github.com>
2022-11-25 22:14:20 +0900

    Add feature
    Only the last selected button remains orange

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 for (var i = 0; i < 5; i++) {
-  document.querySelectorAll(".rating-btn")[i].addEventListener("click", function() {
-    var number = this.innerHTML;
-    document.querySelector("span").innerHTML = number;
-    this.style.backgroundColor = "var(--orange)";
-    this.style.color = "var(--white)";
 
-    if (number > 0) { //submit only when rating button is clicked
+  document.querySelectorAll(".rating-btn")[i].addEventListener("click", function() {
+    var number = Number(this.innerHTML);
+    document.querySelector("span").innerHTML = number;
+
+    for (var i = 0; i < 5; i++) { //changes all the buttons to grey and then changes the clicked button to orange
+      document.querySelectorAll(".rating-btn")[i].style.backgroundColor = "hsl(216, 12%, 25%)";
+      document.querySelectorAll(".rating-btn")[i].style.color = "var(--light-grey)";
+      this.style.backgroundColor = "var(--orange)";
+      this.style.color = "var(--white)";
+    }
+
+    if (number > 0) { //submits only when rating button is clicked
       document.querySelector(".submit-btn").addEventListener("click", function() {
         document.querySelector(".rating-card").classList.add("d-none");
         document.querySelector(".thank-card").classList.remove("d-none");
