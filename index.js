a1857d09eb98420ba24fa706ba90e69a00e3e783
chegx <110959546+chegx@users.noreply.github.com>
2022-11-26 06:25:21 +0900

    Remove redundant

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 for (var i = 0; i < 5; i++) {
 
   document.querySelectorAll(".rating-btn")[i].addEventListener("click", function() {
-    var number = Number(this.innerHTML);
-    document.querySelector("span").innerHTML = number;
-
-    for (var i = 0; i < 5; i++) { 
-      document.querySelectorAll(".rating-btn")[i].classList.remove("selected-btn");
-      this.classList.add("selected-btn");
-    }
+    var number = this.textContent;
+    document.querySelector("span").textContent = number;
 
     if (number > 0) { //submits only when rating button is clicked
       document.querySelector(".submit-btn").addEventListener("click", function() {
