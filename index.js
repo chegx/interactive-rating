72f0ffd47d8c28c3a1b8a33517c8f16d38baf6bc
chegx <110959546+chegx@users.noreply.github.com>
2022-12-11 19:47:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 for (var i = 0; i < 5; i++) {
 
-  document.querySelectorAll(".rating-btn")[i].addEventListener("click", function() {
-    var number = this.textContent;
+  document.querySelectorAll("input")[i].addEventListener("click", function() {
+    var number = this.value;
     document.querySelector("span").textContent = number;
 
-    if (number > 0) { //submits only when rating button is clicked
-      document.querySelector(".submit-btn").addEventListener("click", function() {
+    if (number > 0) { //submits only when radio button is clicked
+      document.querySelector("button").addEventListener("click", function() {
         document.querySelector(".rating-card").classList.add("d-none");
         document.querySelector(".thank-card").classList.remove("d-none");
       });
