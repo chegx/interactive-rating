b101597803d34cc01157973cb98965e570a4ebe2
chegx <110959546+chegx@users.noreply.github.com>
2022-12-12 01:41:25 +0900

    Remove redundant code

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ for (var i = 0; i < 5; i++) {
     var number = this.value;
     document.querySelector("span").textContent = number;
 
-    if (number > 0) { //submits only when radio button is clicked
       document.querySelector("button").addEventListener("click", function() {
         document.querySelector(".rating-card").classList.add("d-none");
         document.querySelector(".thank-card").classList.remove("d-none");
       });
-    }
   });
 }
